1854a69533ba13b5e522149bfcb29544b25655b6
David Gauch <davidgauch@gmail.com>
2022-12-10 16:54:25 -0800

    Day 1

--- aoc01/src/main.rs
+++ aoc01/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, BufRead};
+
+fn main() -> anyhow::Result<()> {
+    let file = File::open("input.txt")?;
+    let lines = io::BufReader::new(file).lines();
+
+    let mut top = [0; 3];
+    let mut curr = 0;
+    for line in lines {
+        let line = line?;
+        if line.is_empty() {
+            if top[0] < curr {
+                top[0] = curr;
+                top.sort();
+            }
+            curr = 0;
+            continue;
+        }
+
+        curr += line.parse::<u32>()?;
+    }
+
+    println!("top 3: {top:?}");
+    println!("sum : {}", top.iter().sum::<u32>());
+
+    Ok(())
 }
