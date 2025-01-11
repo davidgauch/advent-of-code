1ef7f0005c44bc56457115600071b938b2c5d117
David Gauch <davidgauch@gmail.com>
2025-01-10 20:44:20 -0800

    Day 1

--- src/bin/aoc01.rs
+++ src/bin/aoc01.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("aoc01.rs");
+use itertools::Itertools;
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Result},
+};
+
+fn main() -> Result<()> {
+    let (a, b): (Vec<_>, Vec<_>) = BufReader::new(File::open("data/aoc01.data")?)
+        .lines()
+        .filter_map(|l| {
+            let l = l.ok()?;
+            let (a, b) = l.split_once(" ")?;
+
+            Some((a.parse::<usize>().ok()?, b.parse::<usize>().ok()?))
+        })
+        .collect();
+
+    //let sum: u32 = a.into_iter().zip(b).map(|(a, b)| a.abs_diff(b)).sum();
+    //println!("{sum}");
+
+    let counts = b.into_iter().counts();
+    let sum: usize = a
+        .into_iter()
+        .filter_map(|a| Some(a * counts.get(&a)?))
+        .sum();
+
+    println!("{sum}");
+    Ok(())
 }
